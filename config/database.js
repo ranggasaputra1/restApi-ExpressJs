11d1ba3336b262c8883586af72e52d7fa84346cf
ranggasaputra1 <ranggasaputra10092003@gmail.com>
2024-05-06 15:00:00 +0700

    add configuration database

---
+++ config/database.js
@@ -0,0 +1,20 @@
+const mysql = require("mysql");
+
+// Konfigurasi koneksi ke database
+const database = mysql.createConnection({
+  host: "localhost", // Host database
+  user: "root", // Pengguna database
+  password: "", // Kata sandi pengguna database
+  database: "express", // Nama database yang ingin digunakan
+});
+
+// Membuka koneksi ke database
+database.connect((err) => {
+  if (err) {
+    console.error("Koneksi ke database gagal: " + err.stack);
+    return;
+  }
+  console.log("Berhasil Terhubung ke database ");
+});
+
+module.exports = database;
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,10 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
+const database = require("../config/database");
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get("/", function (req, res, next) {
+  res.render("index", { title: "Express" });
 });
 
 module.exports = router;
