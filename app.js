ae5aa8cc45feffa5b8e474fdf17801fbcfe869fa
ranggasaputra1 <ranggasaputra10092003@gmail.com>
2024-05-06 16:16:51 +0700

    add function insert data to database

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ var path = require("path");
 var cookieParser = require("cookie-parser");
 var logger = require("morgan");
 
+//Read ke File nya
 var indexRouter = require("./routes/index");
 var usersRouter = require("./routes/users");
 
@@ -19,6 +20,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 
+//Use ke Route nya
 app.use("/", indexRouter);
 app.use("/users", usersRouter);
 
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,53 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
+var db = require("../config/database");
+var bodyParser = require("body-parser");
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+// Menampilkan data dari database
+router.get("/", (req, res) => {
+  const query = "SELECT * FROM users";
+  db.query(query, (error, results, fields) => {
+    if (error) {
+      console.error("Error in MySQL query: " + error.message);
+      res.status(500).send("Error in MySQL query");
+      return;
+    }
+    res.json({
+      code: "200",
+      status: "OK",
+      message: "Success display data",
+      data: results,
+    });
+  });
 });
 
+// Insert data ke database
+router.post("/", (req, res) => {
+  const { nama, alamat, nohp } = req.body;
+
+  // periksa apakah yang diinputkan sesuai
+  if (!nama || !alamat || !nohp) {
+    return res.status(400).json({
+      code: "400",
+      status: "Bad Request",
+      message:
+        "failed to enter data into the database, please enter the appropriate nama, alamat and nohp",
+    });
+  }
+
+  // kueri untuk menambahkan data baru ke database
+  const query = "INSERT INTO users (nama, alamat, nohp) VALUES (?, ?, ?)";
+  db.query(query, [nama, alamat, nohp], (error, results) => {
+    if (error) {
+      console.error("Failed to add data to database:", error);
+      return res.status(500).json({ error: "Failed to add data to database:" });
+    }
+    res.status(201).json({
+      code: "201",
+      status: "OK",
+      message: "Data has been successfully added to the database",
+      id: results.insertId,
+    });
+  });
+});
 module.exports = router;
